b97836b8a9f46f710a18d9ba9af0497a35970f14
mechvod <33157220+mechvod@users.noreply.github.com>
2017-11-03 21:00:43 +0200

    Update tini.c
    Make tini somewhat daemon-friendly

--- src/tini.c
+++ src/tini.c
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 #define _GNU_SOURCE
+#define SIGNAL_TIMEOUT_SEC 5
+#define SIGNAL_TIMEOUT_NSEC 0
 
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -16,6 +18,12 @@
 
 #include "tiniConfig.h"
 #include "tiniLicense.h"
+
+// The following are needed for work with /proc
+#include <malloc.h>
+#include <sys/vfs.h>
+#include <sys/mount.h>
+#include <dirent.h>
 
 #if TINI_MINIMAL
 #define PRINT_FATAL(...)                         fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n");
@@ -62,7 +70,7 @@ static unsigned int subreaper = 0;
 #endif
 static unsigned int kill_process_group = 0;
 
-static struct timespec ts = { .tv_sec = 1, .tv_nsec = 0 };
+static struct timespec ts = { .tv_sec = SIGNAL_TIMEOUT_SEC, .tv_nsec = SIGNAL_TIMEOUT_NSEC };
 
 static const char reaper_warning[] = "Tini is not running as PID 1 "
 #if HAS_SUBREAPER
@@ -78,6 +86,10 @@ To fix the problem, "
 "or set the environment variable " SUBREAPER_ENV_VAR " to register Tini as a child subreaper, or "
 #endif
 "run Tini as PID 1.";
+
+pid_t (*parse_pid)(char*);
+//this will point to atoi,atol or atoll depending on size of pid_t
+
 
 int restore_signals(const signal_configuration_t* const sigconf_ptr) {
 	if (sigprocmask(SIG_SETMASK, sigconf_ptr->sigmask_ptr, NULL)) {
@@ -129,7 +141,8 @@ int isolate_child() {
 }
 
 
-int spawn(const signal_configuration_t* const sigconf_ptr, char* const argv[], int* const child_pid_ptr) {
+//int spawn(const signal_configuration_t* const sigconf_ptr, char* const argv[], int* const child_pid_ptr) {
+int spawn(const signal_configuration_t* const sigconf_ptr, char* const argv[], pid_t* const child_pid_ptr) {
 	pid_t pid;
 
 	// TODO: check if tini was a foreground process to begin with (it's not OK to "steal" the foreground!")
@@ -389,51 +402,15 @@ int configure_signals(sigset_t* const parent_sigset_ptr, const signal_configurat
 	return 0;
 }
 
-int wait_and_forward_signal(sigset_t const* const parent_sigset_ptr, pid_t const child_pid) {
-	siginfo_t sig;
 
-	if (sigtimedwait(parent_sigset_ptr, &sig, &ts) == -1) {
-		switch (errno) {
-			case EAGAIN:
-				break;
-			case EINTR:
-				break;
-			default:
-				PRINT_FATAL("Unexpected error in sigtimedwait: '%s'", strerror(errno));
-				return 1;
-		}
-	} else {
-		/* There is a signal to handle here */
-		switch (sig.si_signo) {
-			case SIGCHLD:
-				/* Special-cased, as we don't forward SIGCHLD. Instead, we'll
-				 * fallthrough to reaping processes.
-				 */
-				PRINT_DEBUG("Received SIGCHLD");
-				break;
-			default:
-				PRINT_DEBUG("Passing signal: '%s'", strsignal(sig.si_signo));
-				/* Forward anything else */
-				if (kill(kill_process_group ? -child_pid : child_pid, sig.si_signo)) {
-					if (errno == ESRCH) {
-						PRINT_WARNING("Child was dead when forwarding signal");
-					} else {
-						PRINT_FATAL("Unexpected error when forwarding signal: '%s'", strerror(errno));
-						return 1;
-					}
-				}
-				break;
-		}
-	}
-
-	return 0;
-}
-
-int reap_zombies(const pid_t child_pid, int* const child_exitcode_ptr) {
+//int reap_zombies(const pid_t child_pid, int* const child_exitcode_ptr) {
+int reap_zombies(int* const child_exitcode_ptr) {
 	pid_t current_pid;
-	int current_status;
+	int current_status=1; // current_status not only used to keep info
+	                      // about exited process, but also to indicate
+	                      // if we expect a zombie to exist.
 
-	while (1) {
+	while (current_status) {
 		current_pid = waitpid(-1, &current_status, WNOHANG);
 
 		switch (current_pid) {
@@ -441,6 +418,7 @@ int reap_zombies(const pid_t child_pid, int* const child_exitcode_ptr) {
 			case -1:
 				if (errno == ECHILD) {
 					PRINT_TRACE("No child to wait");
+					current_status=0;
 					break;
 				}
 				PRINT_FATAL("Error while waiting for pids: '%s'", strerror(errno));
@@ -448,31 +426,34 @@ int reap_zombies(const pid_t child_pid, int* const child_exitcode_ptr) {
 
 			case 0:
 				PRINT_TRACE("No child to reap");
+				current_status=0;
 				break;
 
 			default:
-				/* A child was reaped. Check whether it's the main one. If it is, then
-				 * set the exit_code, which will cause us to exit once we've reaped everyone else.
+				/* A child was reaped. Check whether it's the
+				 * main one. If it is, then set the exit_code,
+				 * which will cause us to exit once we've reaped
+				 * everyone else.
 				 */
 				PRINT_DEBUG("Reaped child with pid: '%i'", current_pid);
-				if (current_pid == child_pid) {
+//				if (current_pid == child_pid) {
 					if (WIFEXITED(current_status)) {
 						/* Our process exited normally. */
-						PRINT_INFO("Main child exited normally (with status '%i')", WEXITSTATUS(current_status));
+						PRINT_INFO("Child %d exited normally (with status '%i')", current_pid, WEXITSTATUS(current_status));
 						*child_exitcode_ptr = WEXITSTATUS(current_status);
 					} else if (WIFSIGNALED(current_status)) {
 						/* Our process was terminated. Emulate what sh / bash
 						 * would do, which is to return 128 + signal number.
 						 */
-						PRINT_INFO("Main child exited with signal (with signal '%s')", strsignal(WTERMSIG(current_status)));
+						PRINT_INFO("Child %d exited (with signal '%s')", current_pid, strsignal(WTERMSIG(current_status)));
 						*child_exitcode_ptr = 128 + WTERMSIG(current_status);
 					} else {
-						PRINT_FATAL("Main child exited for unknown reason");
+						PRINT_FATAL("Child %d exited for unknown reason", current_pid);
 						return 1;
 					}
-				}
-
+//				}
 				// Check if other childs have been reaped.
+				current_status=1;
 				continue;
 		}
 
@@ -483,13 +464,183 @@ int reap_zombies(const pid_t child_pid, int* const child_exitcode_ptr) {
 	return 0;
 }
 
+int check_pid(char* dirent_name, pid_t* parent) {
+// If caller wants not only check if this entry in /proc na s valid PID,
+// but also to find its parent's PID, then parent must be non-null value
+// pointing to area allocated in advance.
+	int result=1;
+	int count=0;
+	int commlgt=0;
+	char procdatafname[24]="/proc/";
+	// "/proc/"(6 chars) + PID(max. 7 digits) + "/cmdline"(8 chars) + NULL
+	// + some contingencies ;)
+	char buffer_string[32];
+	// PID (max. 7 digits) + space + ( + comm (max. 16 chars) + ) + space
+	// + NULL + some contingencies
+	char onechar;
+	FILE *procdatafile;
+	///PRINT_DEBUG("Started check_pid for /proc/%s",dirent_name);
+	while( result!=0 && dirent_name[count]!=0) {
+		///PRINT_TRACE("checking %d char '%c'(%d)",count,dirent_name[count],dirent_name[count]);
+		// Hope, this is cheaper than atoi - we only check, not parse.
+		if(dirent_name[count]<48 || dirent_name[count]>57) {
+			///PRINT_TRACE("Non-numeric char %d at position %d - not a PID",dirent_name[count],count);
+			result=0;
+		} else {
+			procdatafname[6+count]=dirent_name[count];
+			count++;
+			procdatafname[6+count]=0; // Keep this inside loop in order to
+			                          // have valid null-terminated string
+			                          // for diagnostic printing.
+			///PRINT_TRACE("Building PID directory, chars parsed: %d, dirname: '%s'.",count+1,procdatafname);
+		}
+	} // "while" loop over chars in dirent_name
+	if (result==1) {
+		strcpy(procdatafname+6+count,"/cmdline");
+		///PRINT_TRACE("Going to check if command line in '%s' is non-empty.",procdatafname);
+		procdatafile=fopen(procdatafname,"r");
+		if (procdatafile==NULL) {
+			///PRINT_DEBUG("Failed to open '%s'",procdatafname);
+			result=0;
+		} else {
+			onechar=fgetc(procdatafile);
+			if (feof(procdatafile)) {
+				///PRINT_TRACE("%s is empty",procdatafname);
+				fclose(procdatafile);
+				result=0;
+			} else {
+				//while (!feof(procdatafile) && onechar!='\n') {
+				//	PRINT_TRACE("Read from %s: %c",procdatafname,onechar);
+				//	onechar=fgetc(procdatafile);
+				//}
+				fclose(procdatafile);
+				// cmdline is not empty, now check that state in
+				// /proc/PID/stat is neither "zombie" nor "dead".
+				// To do that we first obtain command from /proc/PID/comm.
+				// Together with PID it gives a string that precedes
+				// the state character in /proc/PID/stat. Having built
+				// the string, we will traverse stat file char-by-char,
+				// to find the state char.
+				strcpy(procdatafname+6+count,"/comm");
+				///PRINT_DEBUG("Going to read command from '%s'",procdatafname);
+				procdatafile=fopen(procdatafname,"r");
+				if (procdatafile==NULL) {
+					///PRINT_DEBUG("failed open %s",procdatafname);
+					result=0;
+				} else {
+					// going to build the string that should
+					// preceed "State" field in /proc/PID/stat
+					strcpy(buffer_string,dirent_name);
+					buffer_string[count]=32;
+					buffer_string[count+1]='(';
+					commlgt=count+2;
+					onechar=fgetc(procdatafile);
+					while (!feof(procdatafile) && onechar!='\n') {
+						buffer_string[commlgt]=onechar;
+						onechar=fgetc(procdatafile);
+						commlgt++;
+						buffer_string[commlgt]=0;
+						///PRINT_TRACE("Chars read: %d; buffer_string='%s'",commlgt+1,buffer_string);
+					}
+					buffer_string[commlgt]=')';
+					commlgt++;
+					buffer_string[commlgt]=' ';
+					commlgt++;
+					buffer_string[commlgt]=0;
+					///PRINT_DEBUG("Resulting buffer_string='%s', length=%d",buffer_string,commlgt);
+					strcpy(procdatafname+6+count,"/stat");
+					///PRINT_DEBUG("Going to read stat file '%s'",procdatafname);
+					procdatafile=fopen(procdatafname,"r");
+					if(procdatafile==NULL){
+						///PRINT_DEBUG("Failed to open '%s'",procdatafname);
+						result=0;
+					} else {
+						onechar=fgetc(procdatafile);
+						count=0;
+						///PRINT_TRACE("start scanning %s",procdatafname);
+						while ( count<commlgt && result==1) {
+							if(onechar!=buffer_string[count] || feof(procdatafile)){
+								///PRINT_DEBUG("Unexpected EOF or unmatched char at position %d: expected '%c', found '%c'",count,buffer_string[count],onechar);
+								result=0;
+							} else {
+								///PRINT_TRACE("Read char '%c' - ok",onechar);
+								onechar=fgetc(procdatafile);
+								count++;
+							}
+						} // loop over chars in buffer_string
+						// Our Holy Grail
+						///PRINT_DEBUG("Process state char: '%c'",onechar);
+						if (onechar=='Z' || onechar=='X' || onechar=='x' ) {
+							///PRINT_DEBUG("Process '%s' is zombie or dead",dirent_name);
+							result=0;
+						} else {
+							PRINT_INFO("Found alive process: '%s'",dirent_name);
+							if (parent!=NULL) {
+								onechar=fgetc(procdatafile); // space
+								onechar=fgetc(procdatafile); // first digit of PPID
+								count=0;
+								while (onechar!=' ') {
+									buffer_string[count]=onechar;
+									onechar=fgetc(procdatafile);
+									count++;
+								}
+								buffer_string[count]=0;
+								PRINT_DEBUG("For child '%s' parent PID string: '%s'",dirent_name,buffer_string);
+								*parent=parse_pid(buffer_string);
+								PRINT_DEBUG("Parsed to number: '%d'",*parent);
+							} // if we need PPID
+						} // if neither zombie nor dead
+					} // if pointer to stat file is not NULL
+				} // if pointer to comm file is not NULL
+			} // if cmdline file is not empty
+		} // if pointer to cmdline file is not NULL
+	} // if dirent_name consists of digits only
+	return result;
+}
+
 
 int main(int argc, char *argv[]) {
-	pid_t child_pid;
+	pid_t child_pid, parent_pid, my_pid=getpid();
+	siginfo_t signal_info;
 
 	// Those are passed to functions to get an exitcode back.
 	int child_exitcode = -1;  // This isn't a valid exitcode, and lets us tell whether the child has exited.
 	int parse_exitcode = 1;   // By default, we exit with 1 if parsing fails.
+
+	int alive_posterity;
+	DIR *procdir;
+	struct dirent *diritem;
+
+	if (sizeof(pid_t)==sizeof(int)) {
+		parse_pid=(pid_t(*)(char*))atoi;
+	} else if (sizeof(pid_t)==sizeof(long)) {
+		parse_pid=(pid_t(*)(char*))atol;
+	}else if (sizeof(pid_t)==sizeof(long long)) {
+		parse_pid=(pid_t(*)(char*))atoll;
+	} else {
+		PRINT_FATAL("Don't know how to parse PID strings\n");
+		return 1;
+	}
+
+	// Check if /proc is mounted, try to mount if it is not.
+	struct statfs *fsdata=(struct statfs *)malloc(sizeof(struct statfs));
+	if (statfs("/proc", fsdata)!=0) {
+		PRINT_FATAL("Could not check if /proc is mounted");
+		return 1;
+	}
+	if (fsdata->f_type!=0x9fa0) {
+		PRINT_INFO("/proc is not of type 0x9fa0 - trying to mount");
+		if (mount ("proc","/proc","proc",0,"")!=0){
+			PRINT_FATAL("Mount /proc failed");
+			return 1;
+		}
+	}
+	free(fsdata);
+	procdir=opendir("/proc");
+	if (procdir==NULL){
+		PRINT_FATAL("Failed to open /proc");
+		return 1;
+	}
 
 	/* Parse command line arguments */
 	char* (*child_args_ptr)[];
@@ -534,22 +685,109 @@ int main(int argc, char *argv[]) {
 	if (spawn_ret) {
 		return spawn_ret;
 	}
+	PRINT_DEBUG("Spawned child '%s' PID=%d",(*child_args_ptr)[0],child_pid);
 	free(child_args_ptr);
 
 	while (1) {
 		/* Wait for one signal, and forward it */
-		if (wait_and_forward_signal(&parent_sigset, child_pid)) {
-			return 1;
-		}
-
+		if (sigtimedwait(&parent_sigset, &signal_info, &ts) == -1) {
+			if (errno!=EAGAIN && errno!=EINTR) {
+				PRINT_FATAL("Unexpected error in sigtimedwait: '%s'", strerror(errno));
+				return 1;
+			}
+		} else {
+			if (signal_info.si_signo!=SIGCHLD) {
+				PRINT_INFO("Received signal %s(%d)",strsignal(signal_info.si_signo),signal_info.si_signo);
+				rewinddir(procdir);
+				errno=0;
+				diritem=readdir(procdir);
+				while (diritem!=NULL) {
+					if(diritem->d_type==DT_DIR) {
+						///PRINT_DEBUG("Checking /proc/%s\n",diritem->d_name);
+						if (check_pid(diritem->d_name,&parent_pid)) {
+							///PRINT_DEBUG("Found alive process PID=%s, PPID=%d",diritem->d_name,parent_pid);
+							if (parent_pid==my_pid) {
+								// This is our direct child,
+								// send the signal to it
+								PRINT_DEBUG("Passing signal '%s' to proc %s", strsignal(signal_info.si_signo),diritem->d_name);
+								child_pid=parse_pid(diritem->d_name);
+								PRINT_DEBUG("Going to send signal %s(%d) to PID %d, group=%d",strsignal(signal_info.si_signo),signal_info.si_signo,child_pid,kill_process_group);
+								if (kill(kill_process_group ? -child_pid : child_pid, signal_info.si_signo)) {
+									if (errno == ESRCH) {
+										PRINT_WARNING("Child was dead when forwarding signal");
+									} else {
+										PRINT_FATAL("Unexpected error when forwarding signal: '%s'", strerror(errno));
+										return 1;
+									}
+								}
+							}
+						} //if check_pid returns non-zero
+					} // if item is a dir
+					diritem=readdir(procdir);
+				} // while(diritem!=NULL)
+				if (errno!=0){
+					// something wrong occured
+					PRINT_DEBUG("Error '%s' occured while scanning /proc - ignoring",strerror(errno));
+					errno=0;
+				}
+			} else {
+			PRINT_DEBUG("Received SIGCHLD");
+			}
+	}
 		/* Now, reap zombies */
-		if (reap_zombies(child_pid, &child_exitcode)) {
+		child_exitcode=-1;
+		if (reap_zombies(&child_exitcode)) {
 			return 1;
 		}
-
 		if (child_exitcode != -1) {
-			PRINT_TRACE("Exiting: child has exited");
-			return child_exitcode;
+			// If child_exitcode was assigned to WEXITSTATUS or WTERMSIG,
+			// i.e. if at least one child was reaped successfully
+			// Here we look into /proc for forked processes and check
+			// if they are zombies or not
+			alive_posterity=0;
+			child_pid=0; // 0 is kernel but we don't care ;)
+			rewinddir(procdir);
+			errno=0;
+			diritem=readdir(procdir);
+			while(diritem!=NULL){
+				if(diritem->d_type==DT_DIR) {
+					///PRINT_DEBUG("Checking /proc/%s\n",diritem->d_name);
+					if (check_pid(diritem->d_name,NULL)) {
+						///PRINT_DEBUG("Found alive process PID=%s",diritem->d_name);
+						if (parse_pid(diritem->d_name)==my_pid) {
+							PRINT_DEBUG("%s - it's me.",diritem->d_name);
+						} else {
+							alive_posterity++;
+							// Setting alive_posterity to 1
+							// might be enough, and testing
+							// it coupled with (diritem!=NULL)
+							// would stop the loop and avoid
+							// useless cycles, but counting
+							// alive processes may be useful
+							// for diagnostics.
+							if(child_pid==0) {
+								// readdir(3) gives entries "as is",
+								// i.e. no sorting is done,
+								// but I see that PIDs are processed
+								// in order.
+								child_pid=parse_pid(diritem->d_name);
+							}
+						} // if is't me
+					} //if check_pid returns non-zero
+				} // if item is a dir
+				diritem=readdir(procdir);
+			} // while(diritem!=NULL)
+			if (errno!=0){
+				// something wrong occured
+				PRINT_DEBUG("An error occured while scanning /proc - ignoring");
+				errno=0;
+				//PRINT_DEBUG("Error occured while scanning /proc - discarding result");
+				//alive_posterity=0;
+			}
+			if (alive_posterity==0) {
+				PRINT_TRACE("Exiting: no children left");
+				return child_exitcode;
+			}
 		}
 	}
 }
